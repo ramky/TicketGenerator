627a19d6a9d3e26ba90b8bc6224574e0ea6a151d
Ram Iyer <ramky.iyer@gmail.com>
2020-12-20 08:16:05 -0500

    adding changes for project

--- src/main/java/com/simplilearn/TicketGenerator/TicketGeneratorApplication.java
+++ src/main/java/com/simplilearn/TicketGenerator/TicketGeneratorApplication.java
@@ -1,13 +1,18 @@
-package com.simplilearn.TicketGenerator;
+package com.simplilearn.ticketGenerator;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class TicketGeneratorApplication {
-
-	public static void main(String[] args) {
-		SpringApplication.run(TicketGeneratorApplication.class, args);
-	}
+    public static double getRandomTicketNumber(){
+   	 double min=1000;
+   	 double max=9999;
+   		 double x = (int)(Math.random()*((max-min)+1))+min;
+   		 return x;
+    }
+    public static void main(String[] args) {
+   	 SpringApplication.run(TicketGeneratorApplication.class, args);
+    }
 
 }
--- src/test/java/com/simplilearn/TicketGenerator/TicketGeneratorApplicationTests.java
+++ src/test/java/com/simplilearn/TicketGenerator/TicketGeneratorApplicationTests.java
@@ -1,13 +1,16 @@
-package com.simplilearn.TicketGenerator;
+package com.simplilearn.ticketGenerator;
 
-import org.junit.jupiter.api.Test;
+import org.junit.Test;
+import static org.junit.Assert.*;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class TicketGeneratorApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    private TicketGeneratorApplication ticketer = new TicketGeneratorApplication();
+    @Test
+    public void testRandomNumber() {
+   	 assertTrue(ticketer.getRandomTicketNumber()>1000&&ticketer.getRandomTicketNumber()<9999);
+    }
 
 }
